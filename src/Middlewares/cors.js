18854ad2ffed0d06d786fc7661612aeee20387b3
Andres <ricardo.gomez.restrepo@gmail.com>
2024-03-18 17:50:35 -0500

    New access URLs added

--- src/Middlewares/cors.js
+++ src/Middlewares/cors.js
@@ -1,6 +1,13 @@
 import cors from 'cors'
 
-const ACCEPT_ORIGIN = ['http://localhost:3000', 'http://localhost:5173']
+const ACCEPT_ORIGIN = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'http://localhost:5173/auth/login',
+  'https://prueba-informa-api-production.up.railway.app',
+  'https://prueba-informa-api-production.up.railway.app/auth/login',
+  'https://prueba-informa-api-production.up.railway.app/tasks',
+]
 
 export const corsMiddleware = ({ acceptOrigin = ACCEPT_ORIGIN } = {}) =>
   cors({
